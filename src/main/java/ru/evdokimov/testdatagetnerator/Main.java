ed6e6a8eafb28e3c5d6a10d30f008bedd40a77a8
EvdokimovViktor <evdokimov.viktor.91@mail.ru>
2021-05-10 18:09:57 +0500

    Add readmy.txt and JCommander

--- src/main/java/ru/evdokimov/testdatagetnerator/Main.java
+++ src/main/java/ru/evdokimov/testdatagetnerator/Main.java
@@ -1,32 +1,37 @@
 package ru.evdokimov.testdatagetnerator;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.validators.PositiveInteger;
 import ru.evdokimov.testdatagetnerator.Entities.Group;
 import ru.evdokimov.testdatagetnerator.Entities.GroupsGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.util.Arrays;
+
 
 
 public class Main {
+    @Parameter(names="-gc", validateWith = PositiveInteger.class)
+     int gc;
+
+    @Parameter(names="-pc", validateWith = PositiveInteger.class)
+     int pc;
+
     public static void main(String[] args) {
-        int groupCount = 25;
-        int personCount = 500;
-        try {
-            if (args.length == 2) {
-                System.out.println("Groups count: " + args[0] + "\nPersons count: " + args[1]);
-                groupCount = Integer.parseInt(args[0]);
-                personCount = Integer.parseInt(args[1]);
-            }
-        } catch (NumberFormatException e){
-            System.out.println("Invalid arguments, please enter 2 positive integer numbers.");
-            throw new RuntimeException(e);
-        }
-        createJsonFile("src/main/resources/Data/", groupCount, personCount);
+        Main main = new Main();
+        JCommander.newBuilder().addObject(main).build().parse(args);
+        main.createJsonFile();
+        System.out.println("Complete");
     }
 
-    public static void createJsonFile (String address, int groupCount, int personCount){
-        GroupsGenerator gg = new GroupsGenerator(address);
+    public void createJsonFile (){
+        int groupCount = 25;
+        int personCount = 500;
+        if(gc > 0) groupCount = gc;
+        if(pc > 0) personCount = pc;
+        System.out.println("Group count: " + groupCount + "\nPerson count: " + personCount);
+        GroupsGenerator gg = new GroupsGenerator("src/main/resources/Data/");
         Group[] groupsArray = gg.generateGroups(groupCount, personCount);
         ObjectMapper obj = new ObjectMapper();
         try {
